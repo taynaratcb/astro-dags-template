ab60624ca0dd45578c8614955d90801de1c61e1f
Taynara Tenório <taynara.tcb@gmail.com>
2025-09-22 10:30:30 -0300

    Update hello_astro.py
    Update the hello world function to print a different message

--- dags/hello_astro.py
+++ dags/hello_astro.py
@@ -7,7 +7,7 @@ from datetime import datetime
 
 @task
 def hello_world():
-    print("Hello World - testing 123")
+    print("Hello World - 1010101010")
 
 @dag(
     dag_id='hello_world',
@@ -23,6 +23,7 @@ def hello_world_dag():
 
 
 dag = hello_world_dag()
+
 
 
 
